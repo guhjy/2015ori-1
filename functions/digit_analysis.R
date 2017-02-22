f580f57ab5efe718ad9f00e61b1b1ea251d7a6ed
CHJ Hartgerink <chjh@protonmail.com>
2017-02-22 18:52:26 +0100

    Revis digit_analysis.R

--- functions/digit_analysis.R
+++ functions/digit_analysis.R
@@ -1,4 +1,15 @@
-digit_analysis <- function(x, type = 'terminal')
+#' Digit analysis
+#'
+#' @param x A vector or matrix of numeric values.
+#' @param type Type of digit analysis ('benford' or 'terminal')
+#'
+#' @return
+#' @export
+#'
+#' @examples digit_analysis(c(1.234, 65.4321, 53.222), type = 'terminal')
+digit_analysis <- function(
+  x,
+  type = 'terminal')
 {
   if(!(is.matrix(x) | is.vector(x)))
   {
@@ -6,45 +17,70 @@ digit_analysis <- function(x, type = 'terminal')
          If specifying a matrix, ensure results from one set of digits are in the 
          rows!')
   }
+  
   if(!(type == 'terminal' | type == 'benford'))
   {
-    stop("Only benford and terminal allowed as types.")
+    stop("Only 'benford' and 'terminal' allowed as types.")
   }
-  
-  matrix_of_digits <- apply(t(x), 1, function(y)
-  {
-    splitted <- strsplit(as.character(y), "")
-    terminal <- lapply(splitted, function(z) tail(z, 1))
-    return(unlist(terminal))
-  })
-  
-  mode(matrix_of_digits) <- "numeric"
-  chi <- apply(matrix_of_digits, 2, function(q)
-  {
-    obs <- digit_counter(q, type = type)
-    exp <- expected_digit_counter(q, type = type)
-    testval <- sum((obs-exp)^2/exp)
-    return(testval)
-  })
   
   if (type == 'terminal')
   {
     df <- 10 - 1 
+    
+    matrix_of_digits <- apply(
+      t(x),
+      1,
+      function(y)
+      {
+        splitted <- strsplit(as.character(y), "")
+        digits <- lapply(splitted, function(z) tail(z, 1))
+        return(unlist(digits))
+      })
   } else if (type == 'benford')
   { 
     df <- 9 - 1
+    
+    matrix_of_digits <- apply(
+      t(x),
+      1,
+      function(y)
+      {
+        splitted <- strsplit(as.character(y), "")
+        digits <- lapply(splitted, function(z) head(z, 1))
+        return(unlist(digits))
+      })
   } else
   {
     stop("Something went awry.")
   }
   
-  pval <- pchisq(q = chi, df = df, lower.tail = FALSE)
-  res <- data.frame(chi, df = df, pval)
+  mode(matrix_of_digits) <- "numeric"
+  
+  chi <- apply(
+    matrix_of_digits,
+    2, 
+    function(q)
+    {
+      obs <- digit_counter(q, type = type)
+      exp <- expected_digit_counter(q, type = type)
+      testval <- sum((obs-exp)^2/exp)
+      return(testval)
+    })
+  
+  pval <- pchisq(
+    q = chi,
+    df = df,
+    lower.tail = FALSE)
+  res <- data.frame(chi,
+                    df = df, 
+                    pval)
   
   return(res)
 }
 
-digit_counter <- function(x, type)
+digit_counter <- function(
+  x,
+  type)
 {
   if(!is.vector(x)) stop("Currently only works with vectors.")
   tabulated <- table(x)
@@ -66,7 +102,9 @@ digit_counter <- function(x, type)
   counts
 }
 
-expected_digit_counter <- function(x, type = type)
+expected_digit_counter <- function(
+  x,
+  type = type)
 {
   if (!is.vector(x)) stop("Currently only works with vectors.")
   if (!(type == 'terminal' | type == 'benford'))
